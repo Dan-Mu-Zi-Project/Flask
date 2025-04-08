610582e05fc096845a387baba599dcb76e68dcb4
Jaewhan Shim <troyshim1@gmail.com>
2025-04-08 16:15:44 +0900

    FIX: 서버 재로드

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    EXTERNAL_SERVER_URL = os.getenv("EXTERNAL_SERVER_URL", "http://localhost:8081")
+    EXTERNAL_SERVER_URL = os.getenv("EXTERNAL_SERVER_URL")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     SWAGGER_ENABLED = False
     DEBUG = False
